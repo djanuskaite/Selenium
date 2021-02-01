6987fad194872b63dbbe23e8631a2d23e06965cd
djanuskaite <januskaite.domante@gmail.com>
2021-02-01 15:33:19 +0200

    Selenium + JUnit

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -6,18 +6,26 @@ import org.junit.Test;
 public class SeleniumTest {
     @Before
     public void doBeforeTest() {
-        Selenium.setUp();
+        Selenium.setup();
     }
 
     @Test
-    public void searchByKeywordTest(){
-        Selenium.searchByKeyword("Baranauskas");
-        compareResultsTest();
+    public void searchKeywordTest(){
+        Selenium.searchKeyword("Baranauskas");
+        //compareResultsNumberTest();
+        compareResultsStringTest();
     }
 
-    public void compareResultsTest() {
-        int results = Selenium.compareResults();
-        Assert.assertEquals(160000, results);
+    public void compareResultsNumberTest() {
+        int results = Selenium.compareResultsNumber();
+        Assert.assertEquals(157000, results);
+    }
+    public void compareResultsStringTest() {
+        String results = Selenium.compareResultString();
+        Assert.assertEquals(".Results", results); // tas pats kas results.equals("Results")
+
+        //jeigu reikia surasti eilutes dali, naudoti contains metoda - results.contains("Results")
+        // tas pats kas sql LIKE %
     }
 
     @After
