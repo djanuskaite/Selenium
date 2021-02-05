42378ac66d8a14d66eafa64fd13e0c31476facf9
djanuskaite <januskaite.domante@gmail.com>
2021-02-05 10:36:58 +0200

    Selenium TestNG + raportas

--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -4,87 +4,102 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Selenium {
+    //browser pasideklaruojam kaip globalu kintamaji
     private static WebDriver browser;
     public static final int WAIT_FOR_TIME = 2;
     public static final String BING_SEARCH_BUTTON_XPATH = "//*[@id=\"sb_form_go\"]";
 
-    public static void main(String args[]) {
+    public static void main(String args[]){
+        //atspausdinam su kokiu irankiu dirbam pirmiausia
         System.out.println("Selenium");
 
-        setup();
-        searchKeyword("Baranauskas");
+
+        setUp();
+
+        searchByKeyword("Baranauskas");
+
         compareResultsNumber();
+
         close();
     }
+    //cia susikuriam atskiras funkcijas konkretiems veiksmams
+    public static void setUp (){
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver87.exe");
 
-    public static void setup() {
-        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver87.exe");
         browser = new ChromeDriver();
         browser.get("https://www.bing.com");
     }
 
-    public static void searchKeyword(String keyword) {
-        waitForVisibilityElementLocated("//*[@id=\"sb_form_q\"]");
+    //siuo atveju keyword - bet koks paieskos zodis - galima ieskoti pagal id, name, xpath - search laukeliui kok priskirtas html'e
+    public static void searchByKeyword (String keyword){
+        waitForVisibilityOfElementLocated("//*[@id=\"sb_form_q\"]");
         WebElement searchField = browser.findElement(By.xpath("//*[@id=\"sb_form_q\"]"));
         searchField.sendKeys(keyword);
-//        searchField.sendKeys(Keys.ENTER);
+//        searchField.sendKeys(Keys.ENTER); //cia simuliujam enter paspaudima
 
         WebElement searchBtn = browser.findElement(By.xpath(BING_SEARCH_BUTTON_XPATH));
 
-        // pirmas veikiantis budas(javascriptExecuter)
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) browser;
-        javascriptExecutor.executeScript("arguments[0].click();", searchBtn);
+        //pirmas veikianti budas
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) browser; //castinimas cia vaziuoja :)
+        javascriptExecutor.executeScript("arguments[0].click()",searchBtn);
 
-//        // antras budas, neveikiantis google ir binge
-//        waitForVisibilityElementLocated(BING_SEARCH_BUTTON_XPATH);
+        //antras budas - neveikia bing ir google
+//        waitForVisibilityOfElementLocated(BING_SEARCH_BUTTON_XPATH);
 //        searchBtn.click();
-//
-//        // trecias budas, neveikiantis google ir binge
+
+        //trecias budas - neveikia google ir bing
 //        waitForElementToBeClickable(BING_SEARCH_BUTTON_XPATH);
 //        searchBtn.click();
     }
 
-    public static String compareResultString() {
-        WebElement countResult = browser.findElement(By.className("sb_count"));
+    public static String compareResultString(){
+        WebElement countResults = browser.findElement(By.className("sb_count"));
 
-        String resultStrWithoutNumbers = countResult.getText()
+        //kaip viena eilute reikia rasyti
+        String resultsStrWithoutNumbers = countResults.getText()
                 .replaceAll("[0-9]", "")
-                .replaceAll("[ ,]", "");    //searchKeyword("Baranauskas");
-        return resultStrWithoutNumbers;
+                .replaceAll("[ ,]","");
+
+        return resultsStrWithoutNumbers;
     }
 
-    public static int compareResultsNumber() {
-        WebElement countResult = browser.findElement(By.className("sb_count"));
+    public static int compareResultsNumber(){
+        WebElement countResults = browser.findElement(By.className("sb_count"));
+        System.out.println(countResults.getText());
 
-        System.out.println(countResult.getText());
-        String resultStr = countResult.getText()
-                .replaceAll("[a-zA-z]", "")
-                .replaceAll("[ ,]", "");    //searchKeyword("Baranauskas");
+        //kaip viena eilute reikia rasyti
+        String results = countResults.getText()
+                .replaceAll("[A-Za-z]", "")
+                .replaceAll("[.  ,]","");
 
-        int resultInt = Integer.parseInt(resultStr);
-        System.out.println(resultInt);
+        //cia konvertuojam is string in integer
+        int results2 = Integer.parseInt(results);
 
-        if (resultInt > 50000) {
-            System.out.println("Rasytojas yra pakankamai zinomnas");
-        } else {
-            System.out.println("Nelabai zinomas");
+        if (results2>50000){
+            System.out.println("Rašytojas yra populiarus!");
+        }else {
+            System.out.println("Nelabai populiarus");
         }
-        return resultInt;
-
+        return results2;
     }
 
-    public static void waitForVisibilityElementLocated(String xpath) {
-        WebDriverWait wait = new WebDriverWait(browser, WAIT_FOR_TIME);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
-    }
-
-    public static void waitForElementToBeClickable(String xpath) {
-        WebDriverWait wait = new WebDriverWait(browser, WAIT_FOR_TIME);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
-    }
-
-
-    public static void close() {
+    public static void close (){
         browser.close();
     }
-}
+
+    public static void waitForVisibilityOfElementLocated(String xPath){
+        WebDriverWait wait = new WebDriverWait(browser, WAIT_FOR_TIME);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath)));
+    }
+
+    public static void waitForElementToBeClickable(String xPath){
+        WebDriverWait wait = new WebDriverWait(browser, WAIT_FOR_TIME);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xPath)));
+    }
+
+    public static void waitForTimeByID(String element){
+        WebDriverWait wait = new WebDriverWait(browser, WAIT_FOR_TIME);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id(element)));
+    }
+
+};
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -2,33 +2,40 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.OrderWith;
+
+
 
 public class SeleniumTest {
     @Before
-    public void doBeforeTest() {
-        Selenium.setup();
+    public void dpBeforeTest() {
+        Selenium.setUp();
     }
 
     @Test
-    public void searchKeywordTest(){
-        Selenium.searchKeyword("Baranauskas");
-        //compareResultsNumberTest();
-        compareResultsStringTest();
+
+    public void searchByKeywordTest() {
+        Selenium.searchByKeyword("Baranauskas");
+        compareResultStringTest();
     }
 
+    //sujungiam cia du testus i viena, nes jie priklauso labai visi nuo kito
+//
     public void compareResultsNumberTest() {
         int results = Selenium.compareResultsNumber();
-        Assert.assertEquals(157000, results);
+        Assert.assertEquals(156000, results);
     }
-    public void compareResultsStringTest() {
-        String results = Selenium.compareResultString();
-        Assert.assertEquals(".Results", results); // tas pats kas results.equals("Results")
 
-        //jeigu reikia surasti eilutes dali, naudoti contains metoda - results.contains("Results")
-        // tas pats kas sql LIKE %
+    public void compareResultStringTest() {
+        String results = Selenium.compareResultString();
+        Assert.assertEquals("Results", results); // tas pats kas - results.equals("Results")
+
+        //jeigu reikia surasti eilutes dali, naudoti "contains" metoda - results.contains("results")
     }
 
     @After
-    public void close() { Selenium.close();}
+    public void close() {
+        Selenium.close();
+    }
 
 }
